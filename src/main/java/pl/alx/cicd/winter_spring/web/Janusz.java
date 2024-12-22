332748159a61e5fc99bf359c8e9abeb5109522ba
Janusz <janusz.chyzy@gmail.com>
2024-12-22 16:41:13 +0100

    upate

--- src/main/java/pl/alx/cicd/winter_spring/web/Janusz.java
+++ src/main/java/pl/alx/cicd/winter_spring/web/Janusz.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Zosia {
+public class Janusz {
 
     @GetMapping("/janusz")
     public String hello() {
