bc325173849edebe15defd56a0cf675ee4fd69aa
Patryk Czarnik <p.czarnik@alx.pl>
2024-12-22 10:23:00 +0100

    Nowy projekt z initializr

---
+++ src/main/java/pl/alx/cicd/winter_spring/WinterSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.alx.cicd.winter_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/alx/cicd/winter_spring/WinterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.alx.cicd.winter_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
